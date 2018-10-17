ed835a93db481758152a7c9a41e9c6b3e2a737ff
tieuconghoa <tieuconghoa193@gmail.com>
2018-10-17 13:35:38 +0700

    Delete index.js

--- config/index.js
+++
@@ -1,7 +0,0 @@
-var configValues = require('./config');
-
-module.exports = {
-    getDbConnectionString: function() {
-        return `mongodb://${configValues.username}:${configValues.password}@ds225253.mlab.com:25253/user`;
-    }
-}
